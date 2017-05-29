fa7805014cfb66e2c7186d91d769d85569025d20
jakubbbbbc <jakub.ciemiega@gmail.com>
2017-05-29 00:57:19 -0500

    Update Tetris.java

--- src/Tetris.java
+++ src/Tetris.java
@@ -8,12 +8,15 @@ import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+
+
+
 public class Tetris extends JFrame implements ActionListener
 {
 	private static final int WIDTH= 700, HEIGHT= 621;
 	
 	private static Board board;
-	private static JLabel score, highScore, paused, next;
+	private static JLabel scoreLabel, highScoreLabel, pausedLabel, next;
 	private JButton newGame;
 	
 	public Tetris()
@@ -24,12 +27,13 @@ public class Tetris extends JFrame implements ActionListener
 		setResizable(false);
 		setLocationRelativeTo(null);
 		
-		score = new JLabel ("Score: 0");
-		highScore= new JLabel ("High Score: 0");
-		paused= new JLabel (" ", JLabel.CENTER);
+		
+		board= new Board();
+		scoreLabel = new JLabel ("Score: 0");
+		highScoreLabel= new JLabel ("High Score: "+board.getHighScore());
+		pausedLabel= new JLabel (" ", JLabel.CENTER);
 		next= new JLabel("Next Shape", JLabel.CENTER);
 		newGame= new JButton ("New Game");
-		board= new Board();
 		
 		
 		// set the panel
@@ -37,10 +41,10 @@ public class Tetris extends JFrame implements ActionListener
 		//right.setSize(200, 621);
 		right.setLayout(new GridLayout(5,1));
 		right.add(next);
-		right.add(paused);
+		right.add(pausedLabel);
 		right.add(newGame);
-		right.add(highScore);
-		right.add(score);
+		right.add(highScoreLabel);
+		right.add(scoreLabel);
 		
 		// set the whole layout
 		setLayout(new GridLayout(1,2));
@@ -59,19 +63,30 @@ public class Tetris extends JFrame implements ActionListener
 	public void actionPerformed (ActionEvent e)
 	{
 		String action= e.getActionCommand();
+		
 		if (action.equals("New Game")) {
 			board.setBoard();
 			board.nextShape();
+			board.setScore(0);
+			scoreLabel.setText("Score: 0");
+			pausedLabel.setText(" ");
 			board.startTimer();
 			//board.setFocusable(true);
 			board.requestFocusInWindow();
+			board.setGameOver(false);
 			System.out.println("start the game");
 		}
 		
 	}
 	
-	public static JLabel getPaused() {
-		return paused;
+	public static JLabel getPausedLabel() {
+		return pausedLabel;
+	}
+	public static JLabel getScoreLabel() {
+		return scoreLabel;
+	}
+	public static JLabel getHighScoreLabel() {
+		return highScoreLabel;
 	}
 	
 	public static void main(String[]args)
@@ -82,5 +97,6 @@ public class Tetris extends JFrame implements ActionListener
 				System.out.print(board.getBoard()[i][j]+"\t");
 			System.out.println("\n");
 			}
+		System.out.println(scoreLabel.getText());
 	}
 }
