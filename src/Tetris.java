03e8539012e017ba75279a417bbbb7ee665d8f8a
jakubbbbbc <jakub.ciemiega@gmail.com>
2017-05-30 14:07:01 -0500

    Update Tetris.java

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,28 +1,30 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JButton;
 import javax.swing.JPanel;
-
-import java.awt.GridLayout;
-import javax.swing.BoxLayout;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 
 
 
 public class Tetris extends JFrame implements ActionListener
 {	
-	private static final int borderWidth=3;
+	private static final int borderWidth=2;
 	private static final int WIDTH= 700, HEIGHT= 622+2*borderWidth; //700, 622
 	
 	private static Board board;
-	private ViewBoard nextBoard;
-	private static JLabel scoreLabel, highScoreLabel, gameOverLabel, pausedLabel, next, levelLabel, linesMadeLabel;
+	private ViewBoard nextBoard, holdBoard;
+	private static JLabel scoreLabel, highScoreLabel, gameOverLabel, pausedLabel, next, levelLabel, linesClearedLabel;
 	private JButton newGame;
 	
 	public int newGameCount=0;
+	public GridBagConstraints c;
 	
 	public Tetris()
 	{
@@ -34,14 +36,14 @@ public class Tetris extends JFrame implements ActionListener
 		
 		// initialize stuff
 		board= new Board();
-		nextBoard= new ViewBoard();
+		nextBoard= new ViewBoard(board.getCurrentShape(), board.getNextShape(), false);
 		scoreLabel = new JLabel ("Score: 0");
 		highScoreLabel= new JLabel ("High Score: "+board.getHighScore());
 		gameOverLabel= new JLabel ("Press \"New Game\" to start a new game.");
 		pausedLabel= new JLabel (" ", JLabel.CENTER);
 		next= new JLabel("Next Shape", JLabel.CENTER);
 		levelLabel= new JLabel ("Level: 1");
-		linesMadeLabel= new JLabel("Lines made: 0");
+		linesClearedLabel= new JLabel("Lines made: 0");
 		newGame= new JButton ("New Game");
 		
 		
@@ -49,16 +51,51 @@ public class Tetris extends JFrame implements ActionListener
 		JPanel right= new JPanel();
 		//right.setSize(200, 621);
 		//right.setLayout(new GridLayout(5,1));
-		right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
-		right.add(next);
-		right.add(nextBoard);
-		right.add(pausedLabel);
-		right.add(newGame);
-		right.add(gameOverLabel);
-		right.add(levelLabel);
-		right.add(linesMadeLabel);
-		right.add(scoreLabel);
-		right.add(highScoreLabel);
+		right.setLayout(new GridBagLayout());
+		c = new GridBagConstraints();
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 2;
+		c.weighty = .1;
+		c.fill = GridBagConstraints.BOTH;
+		right.add(next, c);
+		
+		c.gridy = 1;
+		c.weighty = 1;
+	//	c.insets = new Insets(0, 92, 0, 0);
+		right.add(nextBoard, c);
+		
+		c.gridy = 2;
+		c.weightx = .1;
+		c.weighty = .1;
+	//	c.insets = new Insets(0, 0, 0, 0);
+		c.fill = GridBagConstraints.NONE;
+		right.add(pausedLabel, c);
+		
+		c.gridy = 3;
+		c.ipadx = 40;
+		c.ipady = 40;
+		right.add(newGame, c);
+		
+		c.gridy = 4;
+		c.ipadx = 0;
+		c.ipady = 0;
+		right.add(gameOverLabel, c);
+		
+		c.gridy = 5;
+		c.gridwidth = 1;
+		right.add(levelLabel, c);
+		
+		c.gridy = 6;
+		right.add(linesClearedLabel, c);
+		
+		c.gridx = 1;
+		c.gridy = 5;
+		right.add(scoreLabel, c);
+		
+		c.gridy = 6;
+		right.add(highScoreLabel, c);
 		
 		// set the whole layout
 		setLayout(new GridLayout(1,2));
@@ -68,7 +105,6 @@ public class Tetris extends JFrame implements ActionListener
 		add(right);
 		
 		newGame.addActionListener(this);
-		board.addKeyListener(board);
 		board.setFocusable(true);
 		setVisible(true);
 	}
@@ -80,11 +116,11 @@ public class Tetris extends JFrame implements ActionListener
 		
 		if (action.equals("New Game")) {
 			board.setBoard();
-			
+			board.addKeyListener(board);
 			board.setScore(0);
 			scoreLabel.setText("Score: 0");
 			board.setLinesMade(0);
-			linesMadeLabel.setText("Lines made: 0");
+			linesClearedLabel.setText("Lines cleared: 0");
 			Shape.setLevel(1);
 			levelLabel.setText("Level: 1");
 			pausedLabel.setText(" ");
@@ -120,8 +156,8 @@ public class Tetris extends JFrame implements ActionListener
 	public static JLabel getLevelLabel() {
 		return levelLabel;
 	}
-	public static JLabel getLinesMadeLabel() {
-		return linesMadeLabel;
+	public static JLabel getLinesClearedLabel() {
+		return linesClearedLabel;
 	}
 	
 	public static void main(String[]args)
